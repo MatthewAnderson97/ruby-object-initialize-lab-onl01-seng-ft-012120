57e485aabce7dc78ea376d31a570af9b2d8f76df
Matthew Anderson <anderson.matt1397@gmail.com>
2020-02-02 20:02:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,7 +4,9 @@ class Dog
     @breed = dogs_breed
   end
   def name(dogs_name, dogs_breed = "Mutt")
-    @name
+    @name = dogs_name
+    @breed = dogs_breed
+  end
     def name
       @name
   end
